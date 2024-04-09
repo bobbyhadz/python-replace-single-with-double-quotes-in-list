e44f5f092f709fa4ad56ac1db0f92f2be8a79fdb
bobbyhadz <opmftw@gmail.com>
2024-04-09 15:47:41 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Replacing single with double quotes in a String
+
+# ✅ replaces single with double quotes
+string = "'bobby' 'hadz' 'com'"
+
+result = string.replace("'", '"')
+
+print(result) # 👉️ "bobby" "hadz" "com"
